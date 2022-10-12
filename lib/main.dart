68acf4b4cfca0c3cdffa2b021066703938b185fb
a4sme <a4sme.com@gmail.com>
2022-10-13 01:10:05 +0700

    123

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:myweb/state/home.dart';
+
+final Map<String, WidgetBuilder> map = {
+  '/home': (BuildContext context) => Home(),
+};
+String initialRoute = '/home';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: map,
+      initialRoute: initialRoute,
+    );
+  }
+}
