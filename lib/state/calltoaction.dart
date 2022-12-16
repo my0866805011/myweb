b567ffdfb01b680c4940612f5807839fdcace88c
my0866805011 <my0866805011@gmail.com>
2022-12-16 23:00:26 +0700

    16-12-2022

--- lib/state/calltoaction.dart
+++ lib/state/calltoaction.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:myweb/utility/normalDialog.dart';
 
 class CallToActionLayout extends StatelessWidget {
   // const CallToActionLayout({super.key});
@@ -10,7 +12,7 @@ class CallToActionLayout extends StatelessWidget {
     return Expanded(
       child: Stack(
         children: [
-          newMethod(),
+          newMethod(context),
           Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -32,24 +34,43 @@ class CallToActionLayout extends StatelessWidget {
     );
   }
 
-  Center newMethod() {
+  Center newMethod(BuildContext context) {
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
-        children: [buildUser(), buildPassword(), buildLogin()],
+        children: [
+          buildUser(),
+          buildPassword(),
+          buildLogin(context),
+        ],
       ),
     );
   }
 
-  Container buildLogin() {
+  Container buildLogin(BuildContext context) {
     return Container(
       width: 200,
       child: ElevatedButton(
           onPressed: () {
             print('user = $user, password = $password');
+
+            if ((user == null || user.isEmpty) ||
+                (password == null || password.isEmpty)) {
+              print(' Ihis is space');
+              nomalDialog(context, 'Have Space', 'Plaese Fill Every Blank');
+            } else {
+              print('Ok');
+              checkAuthen();
+            }
           },
           child: Text('Login')),
     );
+  }
+
+  Future<Null> checkAuthen() async {
+    String path =
+        'https://www.57ans.com/myweb/Api/getUserWhereUser.php?isAdd=true&user=$user';
+    await Dio().get(path).then((value) => print('### value = $value'));
   }
 
   Container buildPassword() {
---
+++ lib/utility/normalDialog.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+Future<Null> nomalDialog(
+   context, String title, String message)async{
+  showDialog(
+    context: context,
+    builder: (context) => 
+    SimpleDialog(title: ListTile(
+      leading: Image.asset('assets/images/logo.png'),
+      title: Text(title),
+      subtitle: Text(message),
+      ),children: [TextButton(
+        onPressed: () => Navigator.pop(context), child: Text('Ok'))],
+      )
+    );
+
+}
